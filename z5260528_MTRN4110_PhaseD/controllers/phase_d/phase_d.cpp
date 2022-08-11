8376911a83efd2208d97a54185bd21907f337ec8
calvin360 <61894408+calvin360@users.noreply.github.com>
2022-08-12 04:20:50 +1000

    Add files via upload

---
+++ z5260528_MTRN4110_PhaseD/controllers/phase_d/phase_d.cpp
@@ -0,0 +1,21 @@
+#include <array>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <unistd.h>
+#include <fstream>
+
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+#include <webots/DistanceSensor.hpp>
+#include <webots/Keyboard.hpp>
+#include "phaseA.hpp"
+#include "phaseB.hpp"
+
+int main()
+{
+    runB();
+    runA();
+    return 0;
+}
---
+++ z5260528_MTRN4110_PhaseD/controllers/z5260528_MTRN4110_PhaseA/phaseD.cpp
@@ -0,0 +1,20 @@
+#include <array>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <unistd.h>
+#include <fstream>
+
+#include <webots/Robot.hpp>
+#include <webots/Motor.hpp>
+#include <webots/DistanceSensor.hpp>
+#include <webots/Keyboard.hpp>
+#include "phaseA.h"
+// #include "phaseB.h"
+
+void main()
+{
+    run();
+    return
+}
